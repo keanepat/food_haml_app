a7339159b8074feeb28d63921db7fca6ce40ec53
Patrick Keane <pkeane37@gmail.com>
2014-02-07 12:45:57 -0500

    first commit for haml approach

---
+++ app/models/location.rb
@@ -0,0 +1,10 @@
+class Location < ActiveRecord::Base
+
+	geocoded_by :address
+
+	reverse_geocoded_by :latitude, :longitude
+
+	after_validation :geocode
+
+	after_validation :reverse_geocode
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodHamlApp::Application.config.secret_key_base = '4ad5fb66ce9ff3a10ab46bbb7d5c3831269c7568039d3494095e8b52c8e089086efb20c72e1ea26aea13fdd3b7b261ccbe9c1000da687c4a615caa3e353a28bf'
